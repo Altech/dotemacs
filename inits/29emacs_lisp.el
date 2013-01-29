042915eb55af2f779f6568e3039dde6b466bda16
Altech <takeno.sh@gmail.com>
2013-01-29 10:23:42 +0900

    - improve keybind.

--- inits/29emacs_lisp.el
+++ inits/29emacs_lisp.el
@@ -1,4 +1,4 @@
 (add-hook 'emacs-lisp-mode-hook
           '(lambda ()
-	     (local-set-key (kbd "M-e b") 'eval-buffer)
-	     (local-set-key (kbd "M-e r") 'eval-region)))
+	     (local-set-key (kbd "M-e M-b") 'eval-buffer)
+	     (local-set-key (kbd "M-e M-r") 'eval-region)))
