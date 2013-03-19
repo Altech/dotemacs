8786da8cdb92930019266a4853d29611640d5e84
Altech <takeno.sh@gmail.com>
2013-03-19 18:40:05 +0900

    rename 29emacs_lisp.el -> 29elisp.el

---
+++ inits/29elisp.el
@@ -0,0 +1,6 @@
+(add-hook 'emacs-lisp-mode-hook
+          '(lambda ()
+	     (local-set-key (kbd "M-e M-b") 'eval-buffer)
+	     (local-set-key (kbd "M-e M-r") 'eval-region)
+	     (local-set-key (kbd "\'") 'self-insert-command)
+	     ))
--- inits/29emacs_lisp.el
+++
@@ -1,4 +0,0 @@
-(add-hook 'emacs-lisp-mode-hook
-          '(lambda ()
-	     (local-set-key (kbd "M-e M-b") 'eval-buffer)
-	     (local-set-key (kbd "M-e M-r") 'eval-region)))
