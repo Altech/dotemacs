be5020c342b78564d6f4d09d42080ba257a1407e
Altech <takeno.sh@gmail.com>
2013-04-22 20:40:40 +0900

    introduce ghc-mod and auto-completion in haskell-mode.

--- inits/30haskell.el
+++ inits/30haskell.el
@@ -2,11 +2,7 @@
 (require 'haskell-mode)
 (require 'haskell-cabal)
 
-(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
-(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
-;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
-
+;; ext
 (add-to-list 'auto-mode-alist '("\\.hs$" . haskell-mode))
 (add-to-list 'auto-mode-alist '("\\.lhs$" . literate-haskell-mode))
 (add-to-list 'auto-mode-alist '("\\.cabal\\'" . haskell-cabal-mode))
@@ -15,15 +11,89 @@
 (add-to-list 'interpreter-mode-alist '("runghc" . haskell-mode))     ;#!/usr/bin/env runghc 用
 (add-to-list 'interpreter-mode-alist '("runhaskell" . haskell-mode)) ;#!/usr/bin/env runhaskell 用
 
+(add-hook 'haskell-mode-hook 'turn-on-haskell-doc-mode)
+(add-hook 'haskell-mode-hook 'turn-on-haskell-indentation)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-indent)
+;; (add-hook 'haskell-mode-hook 'turn-on-haskell-simple-indent)
 
 
-;; ;; ghc-mod
-;; ;; cabal でインストールしたライブラリのコマンドが格納されている bin ディレクトリへのパスを exec-path に追加する
-;; (add-to-list 'exec-path (concat (getenv "HOME") "/.cabal/bin"))
-;; ;; ghc-flymake.el などがあるディレクトリ ghc-mod を ~/.emacs.d 以下で管理することにした
-;; (add-to-list 'load-path "~/.emacs.d/elisp/ghc-mod") 
+;; ghc-mod
+;; cabal でインストールしたライブラリのコマンドが格納されている bin ディレクトリへのパスを exec-path に追加する
+(add-to-list 'exec-path (concat (getenv "HOME") "/Library/Haskell/bin"));; ghc-flymake.el などがあるディレクトリ ghc-mod を ~/.emacs.d 以下で管理することにした
+(add-to-list 'load-path "/Users/Altech/Library/Haskell/ghc-7.4.2/lib/ghc-mod-1.12.1/share/")
 
-;; (autoload 'ghc-init "ghc" nil t)
+(autoload 'ghc-init "ghc" nil t)
 
-;; (add-hook 'haskell-mode-hook
-;;   (lambda () (ghc-init)))
+;; (setq enable-local-eval t)
+
+(require 'anything)
+(require 'anything-config)
+(require 'anything-match-plugin)
+
+(defvar anything-c-source-ghc-mod
+  '((name . "ghc-browse-document")
+    (init . anything-c-source-ghc-mod)
+    (candidates-in-buffer)
+    (candidate-number-limit . 9999999)
+    (action ("Open" . anything-c-source-ghc-mod-action))))
+
+(defun anything-c-source-ghc-mod ()
+  (unless (executable-find "ghc-mod")
+    (error "ghc-mod を利用できません。ターミナルで which したり、*scratch* で exec-path を確認したりしましょう"))
+  (let ((buffer (anything-candidate-buffer 'global)))
+    (with-current-buffer buffer
+      (call-process "ghc-mod" nil t t "list"))))
+
+(defun anything-c-source-ghc-mod-action (candidate)
+  (interactive "P")
+  (let* ((pkg (ghc-resolve-package-name candidate)))
+    (anything-aif (and pkg candidate)
+        (ghc-display-document pkg it nil)
+      (message "No document found"))))
+
+(defun anything-ghc-browse-document ()
+  (interactive)
+  (anything anything-c-source-ghc-mod))
+
+;; M-x anything-ghc-browse-document() に対応するキーの割り当て
+;; ghc-mod の設定のあとに書いた方がよいかもしれません
+(add-hook 'haskell-mode-hook
+  (lambda()
+    (setq ac-auto-start 1)
+    (setq ac-auto-show-menu 0.05)
+    (ghc-init)
+    (flymake-mode 1)
+    (define-key haskell-mode-map (kbd "C-M-q") 'anything-ghc-browse-document)
+    (define-key haskell-mode-map (kbd "C-M-j") 'backward-sexp)
+    (define-key haskell-mode-map (kbd "C-M-k") 'kill-sexp)))
+
+
+
+(defun anything-default-display-buffer (buf)
+  (if anything-samewindow
+      (switch-to-buffer buf)
+    (progn
+      (delete-other-windows)
+      (split-window (selected-window) nil t)
+      (pop-to-buffer buf)
+      )))
+
+
+
+;; https://github.com/m2ym/auto-complete
+(ac-define-source ghc-mod
+  '((depends ghc)
+    (candidates . (ghc-select-completion-symbol))
+    (symbol . "s")
+    (cache)))
+
+(defun my-ac-haskell-mode ()
+  (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod)))
+(add-hook 'haskell-mode-hook 'my-ac-haskell-mode)
+
+(defun my-haskell-ac-init ()
+  (when (member (file-name-extension buffer-file-name) '("hs" "lhs"))
+    (auto-complete-mode t)
+    (setq ac-sources '(ac-source-words-in-same-mode-buffers ac-source-dictionary ac-source-ghc-mod))))
+
+(add-hook 'find-file-hook 'my-haskell-ac-init)
