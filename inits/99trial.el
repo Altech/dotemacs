b5435e4fd67fdabb55cef4703d2e1487a1016eea
Altech <takeno.sh@gmail.com>
2013-04-30 23:50:50 +0900

    add current trial.el

--- inits/99trial.el
+++ inits/99trial.el
@@ -1,4 +1,9 @@
-
-
-
+(defun replace-logic-symbol (s e)
+  (interactive "r")
+  (if (use-region-p)
+      (save-restriction 
+	(narrow-to-region s e)
+	(dolist (ls (list (cons "&&" "∧") (cons "||" "∨") (cons "!" "¬") (cons "->" "→")))
+	      (goto-char (point-min))
+	      (replace-string (car ls) (cdr ls))))))
 
