3a76f14f436f42bf34d8533bc90fb172fc407a6d
Altech <takeno.sh@gmail.com>
2013-05-19 14:17:07 +0900

    create minor-mode 'modi-marking-mode and 'modi-presentation-mode.

--- inits/13modification.el
+++ inits/13modification.el
@@ -1,82 +1,99 @@
-(defun altech:rinkou ()
-  (interactive)
-  ;; >><<で囲まれた部分を強調
-  (altech:rinkou-emp-buffer)
-  ;; `C-;`で下線
-  (define-key (current-local-map) (kbd "C-;") 'altech:underline-region)
-  ;; `C-'`で強調
-  (define-key (current-local-map) (kbd "C-'") 'altech:bold-region))
+(define-minor-mode modi-presentation-mode
+  "presentation mode of modi."
+  nil
+  " modi:pres"
+  (list
+   (cons (kbd "C-;") 'modi:underline-region-temporary)
+   (cons (kbd "C-'") 'modi:bold-region-temporary)
+   (cons (kbd "C-.") 'modi:remove-decorations-of-region))
+  (modi:visualize-decorations-of-buffer))
 
-(defun altech:rinkou-emp-buffer ()
-  (interactive)
-  (save-excursion
-    (beginning-of-buffer)
-    (while (re-search-forward "[^>]\\(>>\\) *\\(.+?\\) *\\(<<\\)" nil t)
-      (remove-overlays (match-beginning 0) (match-end 0) 'category 'rinkou)
-      (let ((o (make-overlay (match-beginning 1) (match-end 1))))
-	(overlay-put o 'category 'rinkou)
-	(overlay-put o 'invisible t))
-      (let ((o (make-overlay (match-beginning 3) (match-end 3))))
-	(overlay-put o 'category 'rinkou)
-	(overlay-put o 'invisible t))
-      (let ((o (make-overlay (match-beginning 2) (match-end 2))))
-	(overlay-put o 'category 'rinkou)
-	(overlay-put o 'invisible nil)
-	(overlay-put o 'face 'bold))))
-  (save-excursion
-    (beginning-of-buffer)
-    (while (re-search-forward "[^>]\\(>>>\\) *\\(.+?\\) *\\(<<<\\)" nil t)
-      (remove-overlays (match-beginning 0) (match-end 0) 'category 'rinkou)
-      (let ((o (make-overlay (match-beginning 1) (match-end 1))))
-	(overlay-put o 'category 'rinkou)
-	(overlay-put o 'invisible t))
-      (let ((o (make-overlay (match-beginning 3) (match-end 3))))
-	(overlay-put o 'category 'rinkou)
-	(overlay-put o 'invisible t))
-      (let ((o (make-overlay (match-beginning 2) (match-end 2))))
-	(overlay-put o 'category 'rinkou)
-	(overlay-put o 'invisible nil)
-	(overlay-put o 'face 'underline)))))
+(define-minor-mode modi-marking-mode
+  "presentation mode of modi."
+  nil
+  " modi:mark"
+  (list
+   (cons (kbd "C-;") 'modi:underline-region-permanently)
+   (cons (kbd "C-'") 'modi:bold-region-permanently)
+   (cons (kbd "C-.") 'modi:remove-decorations-of-region))
+  (modi:visualize-decorations-of-buffer))
 
-(defun altech:rinkou-remove-modification (s e)
+(defun modi:visualize-decorations-of-buffer ()
+  (interactive)
+  (flet ((add-face (face-value)
+		   (remove-overlays (match-beginning 0) (match-end 0) 'category 'modification)
+		   (let ((o (make-overlay (match-beginning 1) (match-end 1))))
+		     (overlay-put o 'category 'modification)
+		     (overlay-put o 'invisible t))
+		   (let ((o (make-overlay (match-beginning 3) (match-end 3))))
+		     (overlay-put o 'category 'modification)
+		     (overlay-put o 'invisible t))
+		   (let ((o (make-overlay (match-beginning 2) (match-end 2))))
+		     (overlay-put o 'category 'modification)
+		     (overlay-put o 'invisible nil)
+		     (overlay-put o 'face face-value))))
+    (save-excursion
+      (beginning-of-buffer)
+      (while (re-search-forward "\\(=>-\\)\\(.+?\\)\\(-<=\\)" nil t)
+	(add-face 'bold)))
+    (save-excursion
+      (beginning-of-buffer)
+      (while (re-search-forward "\\(>>>\\)\\(.+?\\)\\(<<<\\)" nil t)
+	(add-face 'underline)))))
+
+(defun modi:remove-decorations-of-region (s e)
   (interactive "r")
   (if (use-region-p)
       (progn
 	(save-restriction
 	  (narrow-to-region s e)
+	  (remove-overlays s e 'category 'modification)
+	  (beginning-of-buffer)
+	  (while (re-search-forward "\\(=>-\\)\\(.+?\\)\\(-<=\\)" nil t)
+	    (replace-match (buffer-substring (match-beginning 2) (match-end 2))))
 	  (beginning-of-buffer)
 	  (while (re-search-forward "\\(>>>\\)\\(.+?\\)\\(<<<\\)" nil t)
-	    (remove-overlays (match-beginning 0) (match-end 0) 'category 'rinkou)
-	    (princ (buffer-substring (match-beginning 2) (match-end 2)))
-	    (replace-match (buffer-substring (match-beginning 2) (match-end 2))))
-	  (while (re-search-forward "\\(>>\\)\\(.+?\\)\\(<<\\)" nil t)
-	    (remove-overlays (match-beginning 0) (match-end 0) 'category 'rinkou)
-	    (princ (buffer-substring (match-beginning 2) (match-end 2)))
-	    (replace-match (buffer-substring (match-beginning 2) (match-end 2))))
-	  ))))
+	    (replace-match (buffer-substring (match-beginning 2) (match-end 2))))))))
 
-(defun altech:rinkou-emp-region ()
+(defun modi:bold-region-permanently ()
   (interactive)
   (let ((modified-p (buffer-modified-p)))
-    (altech:wrap-or-insert ">>" "<<")
+    (modi:wrap-or-insert "=>-" "-<=")
     (save-restriction
       (narrow-to-region (point-at-bol) (point-at-eol))
-      (altech:rinkou-emp-buffer))
-    (unless modified-p
-      (message "save!")
-      (save-buffer))))
-
-(defun altech:rinkou-und-region ()
-  (interactive)
-  (let ((modified-p (buffer-modified-p)))
-    (altech:wrap-or-insert ">>>" "<<<")
-    (save-restriction
-      (narrow-to-region (point-at-bol) (point-at-eol))
-      (altech:rinkou-emp-buffer))
+      (modi:visualize-decorations-of-buffer))
     (unless modified-p
       (save-buffer))))
 
-(defun altech:wrap-or-insert (s1 s2)
+(defun modi:underline-region-permanently ()
+  (interactive)
+  (let ((modified-p (buffer-modified-p)))
+    (modi:wrap-or-insert ">>>" "<<<")
+    (save-restriction
+      (narrow-to-region (point-at-bol) (point-at-eol))
+      (modi:visualize-decorations-of-buffer))
+    (unless modified-p
+      (save-buffer))))
+
+(defun modi:underline-region-temporary (s e)
+  (interactive "r")
+  (if (use-region-p)
+      (let ((o (make-overlay s e)))
+	(overlay-put o 'category 'modification)
+	(overlay-put o 'face 'underline)
+	(keyboard-quit)
+	(message "\n"))))
+
+(defun modi:bold-region-temporary (s e)
+  (interactive "r")
+  (if (use-region-p)
+      (let ((o (make-overlay s e)))
+	(overlay-put o 'category 'modification)
+	(overlay-put o 'face 'bold)
+	(keyboard-quit)
+	(message "\n"))))
+
+(defun modi:wrap-or-insert (s1 s2)
  "Insert the strings S1 and S2.
 If Transient Mark mode is on and a region is active, wrap the strings S1
 and S2 around the region."
@@ -87,21 +104,3 @@ and S2 around the region."
        (goto-char (+ b (length s1)))
        (insert s2))
    (insert s1 s2)))
-
-(defun altech:underline-region (s e)
-  (interactive "r")
-  (if (use-region-p)
-      (let ((o (make-overlay s e)))
-	(overlay-put o 'category 'modification)
-	(overlay-put o 'face 'underline)
-	(keyboard-quit)
-	(message "\n"))))
-
-(defun altech:bold-region (s e)
-  (interactive "r")
-  (if (use-region-p)
-      (let ((o (make-overlay s e)))
-	(overlay-put o 'category 'modification)
-	(overlay-put o 'face 'bold)
-	(keyboard-quit)
-	(message "\n"))))
