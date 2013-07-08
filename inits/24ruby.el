e3e34c6af7892f558918b88567acf387b16f7d15
Altech <takeno.sh@gmail.com>
2013-07-09 06:25:33 +0900

    open .pryrc with ruby-mode.

--- inits/24ruby.el
+++ inits/24ruby.el
@@ -7,6 +7,7 @@
 (add-to-list 'auto-mode-alist '("Gemfile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Rakefile" . ruby-mode))
 (add-to-list 'auto-mode-alist '("gemspec" . ruby-mode))
+(add-to-list 'auto-mode-alist '("\\.pryrc" . ruby-mode))
 
 (setq interpreter-mode-alist (append '(("ruby" . ruby-mode))
                                      interpreter-mode-alist))
