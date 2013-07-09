41cdbeaaf555fbb23eb8889e38cea5265abb5bec
Altech <takeno.sh@gmail.com>
2013-07-09 16:45:03 +0900

    refactor super-coq.

--- inits/31coq.el
+++ inits/31coq.el
@@ -28,6 +28,9 @@
 (defadvice proof-assert-next-command-interactive (after back-one-char)
   (goto-char (1- (point))))
 
+;; ==============================
+;; ** parser and pre-processor **
+;; ==============================
 
 (defun super-coq:parse-goals (s)
  (super-coq:bundle-premises
@@ -85,6 +88,9 @@
       (car ls)
     (super-coq:last (cdr ls))))
 
+;; ==================
+;; ** main command **
+;; ==================
 (defun super-coq:proof-next ()
   (interactive)
   (super-coq:remove-info)
@@ -93,27 +99,34 @@
     (sleep-for 0.15)
     (let* ((after-goals (super-coq:parse-goals (with-current-buffer (get-buffer "*goals*") (buffer-string)))))
       (if (not (equal (cdr (assq 'nsubgoals before-goals)) (cdr (assq 'nsubgoals after-goals))))
-	  ;; show number of subgoals if changed
-	  (super-coq:add-faces-to-goals "^\\([0-9]+\\)"
-	    (overlay-put (make-overlay (match-beginning 1) (match-end 1)) 'face 'underline)
-	    (super-coq:add-before-info (concat " (" (cdr (assq 'nsubgoals before-goals)) ")")))
-	;; emphasize new premises, mark rewrited premises
+	  (super-coq:display-before-nsubgoals)
 	(let ((before-premises (cdr (assq 'premises before-goals)))
 	      (after-premises  (cdr (assq 'premises after-goals))))
-	  (mapc (lambda (premise)
-		  (let ((before-premise (assoc (car premise) before-premises)))
-		    (if (not before-premise)
-			(super-coq:add-faces-to-goals (concat "^ *\\(" (car premise) "\\) : \\(.+\\)")
-			  (dolist (i '(1 2))
-			    (overlay-put (make-overlay (match-beginning i) (match-end i)) 'face 'bold)))
-		      (if (not (equal (cdr premise) (cdr before-premise)))
-			  (super-coq:add-faces-to-goals (concat "^ *\\(" (car premise) "\\) : \\(.+\\)")
-			    (overlay-put (make-overlay (match-beginning 2) (match-end 2)) 'face 'underline)
-			    (goto-char (match-end 2))
-			    (super-coq:add-before-info (concat " (" (cdr before-premise)  ")")))))))
-		after-premises)
-	  ;; show premise by gray if deleted
-	  (mapc (lambda (premise)
+	  (mapc 'super-coq:display-new-or-rewrited-premise after-premises)
+	  (mapc 'super-coq:display-deleted-premise before-premises))
+	(let ((before-goal (cdr (assq 'goal before-goals)))
+	      (after-goal  (cdr (assq 'goal after-goals))))
+	  (super-coq:display-before-goal-if-changed)))
+      (let ((before-ids (super-coq:ids before-goals)) (after-ids (super-coq:ids after-goals)))
+	(super-coq:display-new-subgoals)))))
+
+;; ==========================================
+;; ** display routines (use dynamic scope) **
+;; ==========================================
+
+(defun super-coq:display-new-or-rewrited-premise (premise)
+  (let ((before-premise (assoc (car premise) before-premises)))
+    (if (not before-premise)
+	(super-coq:add-faces-to-goals (concat "^ *\\(" (car premise) "\\) : \\(.+\\)")
+	  (dolist (i '(1 2))
+	    (overlay-put (make-overlay (match-beginning i) (match-end i)) 'face 'bold)))
+      (if (not (equal (cdr premise) (cdr before-premise)))
+	  (super-coq:add-faces-to-goals (concat "^ *\\(" (car premise) "\\) : \\(.+\\)")
+	    (overlay-put (make-overlay (match-beginning 2) (match-end 2)) 'face 'underline)
+	    (goto-char (match-end 2))
+	    (super-coq:add-before-info (concat " (" (cdr before-premise)  ")")))))))
+
+(defun super-coq:display-deleted-premise (premise)
 		  (let ((after-premise (assoc (car premise) after-premises)))
 		    (unless after-premise
 		      (let ((rest (cdr (member premise (reverse before-premises)))))
@@ -125,24 +138,30 @@
 			  (goto-line 3)
 			  (beginning-of-line)
 			  (super-coq:add-before-info (concat "  " (car premise) " : " (cdr premise) "\n")))))))
-		before-premises))
-	;; show before goal term if changed
-	(let ((before-goal (cdr (assq 'goal before-goals)))
-	      (after-goal  (cdr (assq 'goal after-goals))))
-	  (if (not (equal before-goal after-goal))
-	      (super-coq:add-faces-to-goals "==============\n   \\(.+\\)"
-		(goto-char (match-beginning 1))
-		(super-coq:add-before-info (concat before-goal "\n-> "))))))
-      ;; emphasize new subgoal
-      (let ((before-ids (super-coq:ids before-goals)) (after-ids (super-coq:ids after-goals)))
-	(super-coq:add-faces-to-goals "subgoal [0-9]+ (ID [0-9]+)$"
-	  (overlay-put (make-overlay (match-beginning 0) (match-end 0)) 'face 'bold))
-	(mapc (lambda (id)
-		(unless (member id before-ids)
-		  (super-coq:add-faces-to-goals (concat "^.*(ID " id ").*:$")
-		    (overlay-put (make-overlay (match-beginning 0) (match-end 0)) 'face 'bold))))
-	      after-ids)))))
 
+(defun super-coq:display-before-goal-if-changed ()
+  (if (not (equal before-goal after-goal))
+      (super-coq:add-faces-to-goals "==============\n   \\(.+\\)"
+	(goto-char (match-beginning 1))
+	(super-coq:add-before-info (concat before-goal "\n-> ")))))
+
+(defun super-coq:display-before-nsubgoals ()
+  (super-coq:add-faces-to-goals "^\\([0-9]+\\)"
+	    (overlay-put (make-overlay (match-beginning 1) (match-end 1)) 'face 'underline)
+	    (super-coq:add-before-info (concat " (" (cdr (assq 'nsubgoals before-goals)) ")"))))
+
+(defun super-coq:display-new-subgoals ()
+  (super-coq:add-faces-to-goals "subgoal [0-9]+ (ID [0-9]+)$"
+    (overlay-put (make-overlay (match-beginning 0) (match-end 0)) 'face 'bold))
+  (mapc (lambda (id)
+	  (unless (member id before-ids)
+	    (super-coq:add-faces-to-goals (concat "^.*(ID " id ").*:$")
+	      (overlay-put (make-overlay (match-beginning 0) (match-end 0)) 'face 'bold))))
+	after-ids))
+
+;; ==========================
+;; ** modificatin routines **
+;; ==========================
 (defun super-coq:add-before-info (info)
   (let* ((s (concat ">>>" info "<<<")) (p1 (point)) (p2 (+ p1 (length s))))
     (insert s)
