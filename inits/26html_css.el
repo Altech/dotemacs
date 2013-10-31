fbff82036c05fcfee5342d4d5de7032ffd1c8176
Altech <takeno.sh@gmail.com>
2013-10-31 13:24:11 +0900

    add commands  'slim-indent-region-deeply and 'slim-indent-region-deeply.

--- inits/26html_css.el
+++ inits/26html_css.el
@@ -1,3 +1,4 @@
+
 ;; for HTML
 ;; (require 'zencoding-mode) 
 ;; (add-hook 'zencoding-mode-hook
@@ -16,6 +17,38 @@
               (lambda ()
                 (setq css-indent-offset 2)
                 ))
+
+(add-hook 'slim-mode-hook
+	  (lambda ()
+	    (local-set-key (kbd "<M-right>") 'slim-indent-region-deeply)
+	    (local-set-key (kbd "<M-left>") 'slim-indent-region-shallowly)))
+
+(defun slim-indent-region-deeply (s e)
+  (interactive "r")
+  (if (use-region-p)
+      (progn
+	(save-restriction
+	  (narrow-to-region s e)
+	  (let ((buf-lines (count-lines (point-min) (point-max))))
+	    (beginning-of-buffer)
+	    (dotimes (i buf-lines)
+	      (insert "  ")
+	      (next-line)
+	      (beginning-of-line)))))))
+
+
+(defun slim-indent-region-shallowly (s e)
+  (interactive "r")
+  (if (use-region-p)
+      (progn
+	(save-restriction
+	  (narrow-to-region s e)
+	  (let ((buf-lines (count-lines (point-min) (point-max))))
+	    (beginning-of-buffer)
+	    (dotimes (i buf-lines)
+	      (delete-char 2) 
+	      (next-line)
+	      (beginning-of-line)))))))
 
 ;; for LESS
 (require 'less-css-mode)
