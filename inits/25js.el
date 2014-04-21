580d4207fd28f5eea9f59073a7557267fe334d2f
Altech <takeno.sh@gmail.com>
2014-04-21 14:49:08 +0900

    update js2-mode and its configuration

--- inits/25js.el
+++ inits/25js.el
@@ -1,63 +1,11 @@
 ;; for JavaScript
-;;  - js2
-(add-to-list 'load-path "~/.emacs.d/js/")
-(setq-default c-basic-offset 4)
-;; http://d.hatena.ne.jp/speg03/20091011/1255244329
-(autoload 'js2-mode "js2" nil t)
-(add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
+(require 'js2-mode)
+(add-to-list 'auto-mode-alist '("\\.js\\'" . js2-mode))
 
 (add-hook 'js2-mode-hook
-          (lambda ()
-	    (setq js2-cleanup-whitespace nil
-		  js2-mirror-mode nil
-		  js2-bounce-indent-flag nil)
-	    (defun indent-and-back-to-indentation ()
-	      (interactive)
-	      (indent-for-tab-command)
-	      (let ((point-of-indentation
-		     (save-excursion
-		       (back-to-indentation)
-		       (point))))
-		(skip-chars-forward "\s " point-of-indentation)))
-	    (define-key js2-mode-map (kbd "C-i") 'indent-and-back-to-indentation)
-	    ;; (define-key js2-mode-map (kbd "C-m") nil)
-            ))
-
-
-; fixing indentation
-; refer to http://mihai.bazon.net/projects/editing-javascript-with-emacs-js2-mode
-(autoload 'espresso-mode "espresso")
-
-(defun my-js2-indent-function ()
-  (interactive)
-  (save-restriction
-    (widen)
-    (let* ((inhibit-point-motion-hooks t)
-           (parse-status (save-excursion (syntax-ppss (point-at-bol))))
-           (offset (- (current-column) (current-indentation)))
-           (indentation (espresso--proper-indentation parse-status))
-           node)
-
-      (save-excursion
-
-        ;; I like to indent case and labels to half of the tab width
-        (back-to-indentation)
-        (if (looking-at "case\\s-")
-            (setq indentation (+ indentation (/ espresso-indent-level 2))))
-
-        ;; consecutive declarations in a var statement are nice if
-        ;; properly aligned, i.e:
-        ;;
-        ;; var foo = "bar",
-        ;;     bar = "foo";
-        (setq node (js2-node-at-point))
-        (when (and node
-                   (= js2-NAME (js2-node-type node))
-                   (= js2-VAR (js2-node-type (js2-node-parent node))))
-          (setq indentation (+ 2 indentation))))
-
-      (indent-line-to indentation)
-      (when (> offset 0) (forward-char offset)))))
+          #'(lambda ()
+              (local-set-key (kbd "M-j") 'backward-word)
+              ))
 
 (defun my-indent-sexp ()
   (interactive)
@@ -82,33 +30,6 @@
           (forward-line))
         (run-with-timer 0.5 nil '(lambda(ovl)
                                    (delete-overlay ovl)) ovl)))))
-
-(defun my-js2-mode-hook ()
-  (require 'espresso)
-  (setq espresso-indent-level 2
-        indent-tabs-mode nil
-        c-basic-offset 2)
-  (c-toggle-auto-state 0)
-  (c-toggle-hungry-state 1)
-  (set (make-local-variable 'indent-line-function) 'my-js2-indent-function)
-  ; (define-key js2-mode-map [(meta control |)] 'cperl-lineup)
-  (define-key js2-mode-map (kbd "C-\\ M-\\")
-    '(lambda()
-       (interactive)
-       (insert "/* -----[ ")
-       (save-excursion
-         (insert " ]----- */"))
-       ))
-  (define-key js2-mode-map (kbd "C-m") 'newline-and-indent)
-  ; (define-key js2-mode-map [(backspace)] 'c-electric-backspace)
-  ; (define-key js2-mode-map [(control d)] 'c-electric-delete-forward)
-  (define-key js2-mode-map (kbd "C-\\ M-q") 'my-indent-sexp)
-  (if (featurep 'js2-highlight-vars)
-      (js2-highlight-vars-mode))
-  (message "My JS2 hook"))
-
-(add-hook 'js2-mode-hook 'my-js2-mode-hook)
-
 
 ;;  - ejacs
 (add-to-list 'load-path "~/.emacs.d/ejacs/")
