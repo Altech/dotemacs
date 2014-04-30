ce19bb6db64614876649619d370075d58d84ff9d
Altech <takeno.sh@gmail.com>
2014-04-30 12:35:06 +0900

    Add ever-mode as submodule
    - and remove hascm-mode.

--- inits/99trial.el
+++ inits/99trial.el
@@ -6,6 +6,3 @@
 	(dolist (ls (list (cons "&&" "∧") (cons "||" "∨") (cons "!" "¬") (cons "->" "→")))
 	      (goto-char (point-min))
 	      (replace-string (car ls) (cdr ls))))))
-
-
-(load "/Users/Altech/dev/scheme/haskell/elisp/hascm-mode.el")
