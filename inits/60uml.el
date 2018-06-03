28d0740ec92fe9b578ba2b2c7257181b00d6f9fb
Altech <takeno.sh@gmail.com>
2018-06-03 20:01:37 +0900

    Print as png

--- inits/60uml.el
+++ inits/60uml.el
@@ -2,4 +2,5 @@
 (require 'plantuml-mode)
 
 (setq plantuml-jar-path (expand-file-name "~/.emacs.d/bin/plantuml.jar"))
+(setq plantuml-output-type "png")
 (add-to-list 'auto-mode-alist '("\\.plantuml\\'" . plantuml-mode))
