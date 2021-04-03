01c620b5d8111c6cdaf244b7b71ae308b582f148
Altech <takeno.sh@gmail.com>
2021-04-03 15:18:56 +0900

    Replace quote

--- init.el
+++ init.el
@@ -9,8 +9,7 @@
  ;; If there is more than one, they won't work right.
  '(js2-strict-trailing-comma-warning nil)
  '(package-selected-packages
-   (quote
-    (helm column-marker dash-at-point helm-open-github auto-complete highlight-symbol dropdown-list git-gutter magit rbenv popwin haml-mode color-theme plantuml-mode flymake-json json-mode lua-mode swift-mode go-eldoc go-mode coffee-mode erlang elixir-mode haskell-mode lispxmp slime paredit slim-mode scss-mode js2-mode back-button quickrun markdown-mode dired-details yaml-mode anything key-chord fullframe))))
+   '(helm column-marker dash-at-point helm-open-github auto-complete highlight-symbol dropdown-list git-gutter magit rbenv popwin haml-mode color-theme plantuml-mode flymake-json json-mode lua-mode swift-mode go-eldoc go-mode coffee-mode erlang elixir-mode haskell-mode lispxmp slime paredit slim-mode scss-mode js2-mode back-button quickrun markdown-mode dired-details yaml-mode anything key-chord fullframe)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
